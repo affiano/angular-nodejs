cead42e435492f8f8e72793e33fb56c78f9609b7
rjholo <rjholo@outlook.com>
2021-07-22 13:37:17 +0530

    added server configuration

---
+++ backend/app.js
@@ -0,0 +1,59 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+require('dotenv').config()
+
+var app = express();
+const cors = require("cors");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// The user must not forget to summon the sync () method
+const db = require("./models");
+db.sequelize.sync();
+
+// If existing tables need to dropped and the database resynced, enter force: true as the following code
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");  
+//   });
+
+// cors
+var corsOptions = {
+  origin: "http://localhost:5000"
+};
+
+app.use(cors(corsOptions));
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ backend/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASS,
+    DB_NAME: process.env.DB_NAME,
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+
+};
